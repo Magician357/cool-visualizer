4691a155843c7afbfedbd897a2774b107cd59c1a
Magic Man <65839659+Magician357@users.noreply.github.com>
2025-06-14 20:12:02 -0700

    more

--- main.js
+++ main.js
@@ -2,6 +2,16 @@ const canvas = document.getElementById("main");
 const ctx = canvas.getContext("2d");
 // canvas is 480x480
 
+document.getElementById('audio_file').addEventListener('change', async function(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const objectURL = URL.createObjectURL(file);
+        audio_element.src = objectURL;
+    }
+});
+
+
+
 const audio_element = document.getElementById("audio");
 
 const audio_ctx = new (window.AudioContext || window.webkitAudioContext)();
@@ -22,7 +32,7 @@ audio_element.addEventListener("play", () => {
     }
     });
 
-function draw_audio_line(reset=false) {
+function draw_audio_line() {
     analyser.getByteTimeDomainData(data_array);
 
     ctx.globalCompositeOperation = "difference";
@@ -49,17 +59,54 @@ function draw_audio_line(reset=false) {
     ctx.stroke();
 }
 
-const font = "48px monospace";
-const letter_height = 40;
+var Img = [];
+for (var i = 0; i < 30; i++) {
+    Img[i] = new Image();
+    Img[i].src = "images/field/"+i+".gif";
+    // let temp = new Image();
+    // temp.onload = () => {
+    //     console.log(i,"loaded")
+    //     Img[i] = document.createElement("canvas");
+    //     Img[i].width = 480;
+    //     Img[i].height = 480;
+    //     Img[i].getContext("2d").drawImage(temp,0,0);
+    // }
+    // temp.src = "images/field/"+i+".gif";
 
-const display_text = "▢▣▤▥▦▧▨▩◧◨◩◪◫◰◱◲";
+}
+
+var Img2 = [];
+for (var i = 0; i < 6; i++) {
+    Img2[i] = new Image();
+    Img2[i].src = "images/water/"+i+".gif";
+}
+
+const font = "24px Helvetica";
+const letter_height = 30;//40;
+
+const text_input = document.getElementById("text_input");
+
+var display_text = "▢▣▤▥▦▧▨▩◧◨◩◪◫◰◱◲▢▣▤▥▦▧▨▩SORAMIMICAKE";
+
+text_input.value = display_text;
+document.getElementById("text_button").addEventListener("click",(ev)=>{
+    display_text = text_input.value;
+});
 
 var running = true;
 var n = 0;
+var time_offset = 0;
+var reset_time = false;
 function main_loop(now=0){
 
+    if (reset_time){
+        reset_time = false;
+        time_offset = now;
+    }
+    now-=time_offset;
+
     // let test_text = "▢▣▤▥▦▧▨▩◧◨◩◪◫◰◱◲";
-    let offset = Math.floor(now/200);
+    let offset = Math.floor(now/120);
 
     ctx.globalCompositeOperation = "source-over";
 
@@ -70,30 +117,46 @@ function main_loop(now=0){
     ctx.rect(0,0,480,480);
     ctx.fill();
 
-    ctx.fillStyle="white";
+    ctx.drawImage(Img[Math.floor(n/11)%Img.length],0,0);
+    ctx.drawImage(Img[Math.floor(n/10)%Img.length],0,250);
+
+    ctx.drawImage(Img2[Math.floor(n/8)%6],0,0,80,440,0,0,80,480);
+
+    ctx.fillStyle="black";
     ctx.beginPath();
-    ctx.rect(10,0,41,480);
+    ctx.rect(0,110,480,240);
     ctx.fill();
 
+    ctx.globalCompositeOperation = "difference";
+    ctx.fillStyle="white";
+    ctx.beginPath();
+    // ctx.rect(10,110,41,240);
+    ctx.rect(0,110,80,240)
+    ctx.fill();
+
+    ctx.globalCompositeOperation = "source-over";
     ctx.font = font;
     ctx.fillStyle = "black";
     for (let i = 0; i < display_text.length; i++) {
         const char = display_text[(i + offset) % display_text.length];
         const metrics = ctx.measureText(char);
         const charWidth = metrics.width;
-        const x = 30 - charWidth / 2; // Center the character around x = 10
-        const y = (letter_height * i + now / 7) % Math.max(480 + letter_height, letter_height * display_text.length);
+        const x = 37 - charWidth / 2; // Center the character around x = 10
+        const n = Math.max(Math.ceil((letter_height * display_text.length - 480) / letter_height),1)+1;
+        const y = (letter_height * i + now / 7) % (480 + letter_height * n);    
+
         ctx.fillText(char, x, y);
     }
 
-    // ctx.globalCompositeOperation = "difference";
-    // ctx.fillStyle = "white";
-    // ctx.font = "700px monospace";
-    // ctx.fillText("Whats up?",0,480)
+    ctx.globalCompositeOperation = "difference";
+    ctx.fillStyle = "white";
+    ctx.font = "150px Helvetica";
+    ctx.fillText((audio_element.currentTime%60).toFixed(2).padStart(5,"0"),80,470)
+    ctx.fillText((audio_element.currentTime/60).toFixed(2).padStart(5,"0"),80,105)
 
-    draw_audio_line(n<20);
-
-    n+=1;
+    draw_audio_line();
+    // n+=1;
+    n = Math.floor(now*60/1000)
 
     if (running){
         window.requestAnimationFrame(main_loop)
